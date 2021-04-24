284a986c829bce7daad835fbc49c8dbdbdc59644
Rodolfo Rodrigues <rudwolf@gmail.com>
2021-04-23 23:15:09 -0300

    Aula 4 Finalizada

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,7 @@
+import Link from "next/link";
 import format from "date-fns/format";
 import ptBR from "date-fns/locale/pt-BR";
+
 import styles from "./styles.module.scss";
 
 export function Header() {
@@ -9,7 +11,9 @@ export function Header() {
   
   return (
     <header className={styles.headerContainer}>
-      <img src="/logo.svg" alt="Podcastr" />
+      <Link href="/">
+        <img src="/logo.svg" alt="Podcastr" />
+      </Link>
       <p>O melhor para você ouvir, sempre</p>
       <span>{currentDate}</span>
     </header>
